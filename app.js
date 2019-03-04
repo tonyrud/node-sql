fbe973c510c8ed76ce0dd392dbf6f2503033e951
Tony Rudny <tony_rudny@sweetwater.com>
2019-03-03 19:03:14 -0500

    feat: setup ejs templating and styles

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const express = require('express');
+const { promisify } = require('util');
+// const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const connection = require('./db/dbConnection')();
+
+const sqlQuery = promisify(connection.query).bind(connection);
 const app = express();
-const connection = require('./dbConnection')();
 
 const PORT = process.env.PORT;
 
-const query = 'SELECT COUNT(*) FROM users';
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-connection.query(query, (error, results, fields) => {
-    if (error) throw error;
+app.get('/', async (req, res) => {
+    const countQuery = 'SELECT COUNT(*) as count FROM users';
+    const [usersCount] = await sqlQuery(countQuery);
 
-    console.log('result: ', results);
+    res.render('home', { count: usersCount.count });
 });
 
-connection.end();
-
-app.get('/', (req, res) => {
-    res.send('Hello from our web app!');
+app.post('/register', async (req, res) => {
+    const email = req.body.email;
+    await sqlQuery('INSERT INTO users SET ?', { email });
+    res.redirect('/');
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
